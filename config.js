c7da18d683fb8a44e533b2460d5cf24d7a64cf1d
jccausey <jason@jcausey.com>
2026-07-27 15:05:34 -0400

    adding config.js

---
+++ config.js
@@ -0,0 +1,11 @@
+// config.js
+const firebaseConfig = {
+    apiKey: "AIzaSyDfKjxmmy6NLAApbS9VCdrgrvbQBTPFVTI",
+    authDomain: "thisorthat-b4ec4.firebaseapp.com",
+    databaseURL: "https://thisorthat-b4ec4-default-rtdb.firebaseio.com",
+    projectId: "thisorthat-b4ec4",
+    storageBucket: "thisorthat-b4ec4.firebasestorage.app",
+    messagingSenderId: "872710535957",
+    appId: "1:872710535957:web:3d7d27d1dc652336b7871c",
+    measurementId: "G-ZGH9415L17"
+};
